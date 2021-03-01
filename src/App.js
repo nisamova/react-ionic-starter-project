adb32d07311eb352fa702367a425bce1abb9b4f3
Nazokat Isamova <nazokatardasher@macbook-pro.home>
2021-03-01 10:13:43 -0700

    Starter Cleaned project for Ionic-React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react"
+import {
+  IonButton,
+  IonToast,
+  IonIcon,
+  IonApp,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonTitle
+} from "@ionic/react"
+import { play as playIcon } from "ionicons/icons"
 function App() {
+  const [showToast, setShowToast] = useState(false)
+  const handleClick = () => {
+    setShowToast(true)
+    setTimeout(() => setShowToast(false), 1500)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <IonApp>
+      <IonToolbar>
+        <IonHeader>
+          <IonTitle>My App</IonTitle>
+        </IonHeader>
+      </IonToolbar>
+      <IonContent>
+        <IonButton onClick={handleClick}>
+          <IonIcon icon={playIcon} slot="start" />
+          Click Me
+        </IonButton>
+        <IonToast isOpen={showToast} message="Hello world!" />
+      </IonContent>
+    </IonApp>
+  )
 }
 
-export default App;
+export default App
